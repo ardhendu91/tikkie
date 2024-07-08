61e502acfbd6e707174d49d580a59ce7bba8c374
Ardhendu Mondal <ardhendu91@gmail.com>
2024-07-08 18:54:39 +0200

    adding logs

--- modules/person/personService.ts
+++ modules/person/personService.ts
@@ -4,6 +4,7 @@ class personService {
     static async trial(req: Request): Promise<string | Error> {
         try {
             const message = `Hello! Welcome to the trial!`;
+            console.log('Message:', message);
             return message;
         } catch (err) {
             console.error('Some error occurred', err);
